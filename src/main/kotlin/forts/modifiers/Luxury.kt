08c6b3412db0b5f19b8c23ed62eb8bfbb624d686
buj <buj351@outlook.com>
2025-03-01 21:59:51 +0700

    fix: luxury increases build speed to counteract increase in build cost

--- src/main/kotlin/forts/modifiers/Luxury.kt
+++ src/main/kotlin/forts/modifiers/Luxury.kt
@@ -10,6 +10,7 @@ class Luxury: Modifier() {
 
     override fun start() {
         Vars.state.rules.buildCostMultiplier *= 2f
+        Vars.state.rules.buildSpeedMultiplier *= 2f
         Core.app.post {
             Team.all.forEach {
                 if (!it.active()) return@forEach
@@ -24,5 +25,6 @@ class Luxury: Modifier() {
 
     override fun destroy() {
         Vars.state.rules.buildCostMultiplier /= 2f
+        Vars.state.rules.buildSpeedMultiplier /= 2f
     }
 }
