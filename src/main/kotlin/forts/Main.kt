ce3d11209a1c49dfeabdd48442da55fc606e5866
buj <buj351@outlook.com>
2025-03-09 22:20:31 +0700

    balance: nerfed titan again

--- src/main/kotlin/forts/Main.kt
+++ src/main/kotlin/forts/Main.kt
@@ -180,10 +180,10 @@ class Main: Plugin() {
         (Blocks.cyclone as ItemTurret).ammoTypes.get(Items.plastanium).splashDamage = 95f
         (Blocks.cyclone as ItemTurret).ammoTypes.get(Items.surgeAlloy).splashDamage = 125f
 
-        (Blocks.titan as ItemTurret).ammoTypes.get(Items.thorium).buildingDamageMultiplier = 0.22f
-        (Blocks.titan as ItemTurret).ammoTypes.get(Items.thorium).splashDamage = 230f
+        (Blocks.titan as ItemTurret).ammoTypes.get(Items.thorium).buildingDamageMultiplier = 0.1f
+        (Blocks.titan as ItemTurret).ammoTypes.get(Items.thorium).splashDamage = 350f
         (Blocks.titan as ItemTurret).ammoTypes.get(Items.thorium).splashDamagePierce = true
-        (Blocks.titan as ItemTurret).ammoTypes.get(Items.thorium).splashDamageRadius = 65f
+        (Blocks.titan as ItemTurret).ammoTypes.get(Items.thorium).splashDamageRadius = 60f
         (Blocks.titan as ItemTurret).reload = 180f
     }
 
